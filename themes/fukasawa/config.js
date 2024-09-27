d6c043a83d85ce628891e317f366dcd685f8ed05
zhongzhihai <zhongzhihai@hotmail.com>
2024-09-27 14:44:01 +0800

    Update config.js
    sidebar

--- themes/fukasawa/config.js
+++ themes/fukasawa/config.js
@@ -13,6 +13,6 @@ const CONFIG = {
 
   FUKASAWA_SIDEBAR_COLLAPSE_BUTTON: false, // 侧边栏折叠按钮
   FUKASAWA_SIDEBAR_COLLAPSE_SATUS_DEFAULT: false, // 侧边栏默认折叠收起
-  FUKASAWA_SIDEBAR_COLLAPSE_ON_SCROLL: false // 侧边栏滚动时折叠 仅文章阅读页有效
+  FUKASAWA_SIDEBAR_COLLAPSE_ON_SCROLL: true // 侧边栏滚动时折叠 仅文章阅读页有效
 }
 export default CONFIG
