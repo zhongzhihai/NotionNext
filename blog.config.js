816b6a522dd2b4c414659a8675a4e80077e5aeb6
zhongzhihai <zhongzhihai@hotmail.com>
2024-09-28 23:00:34 +0800

    Update blog.config.js
    SEO

--- blog.config.js
+++ blog.config.js
@@ -24,10 +24,10 @@ const BLOG = {
 
   CUSTOM_MENU: process.env.NEXT_PUBLIC_CUSTOM_MENU || false, // 支持Menu 类型，从3.12.0版本起，各主题将逐步支持灵活的二级菜单配置，替代了原来的Page类型，此配置是试验功能、默认关闭。
 
-  AUTHOR: process.env.NEXT_PUBLIC_AUTHOR || '出海101', // 您的昵称 例如 tangly1024
+  AUTHOR: process.env.NEXT_PUBLIC_AUTHOR || '出国101', // 您的昵称 例如 tangly1024
   BIO: process.env.NEXT_PUBLIC_BIO || '风景这边独好', // 作者简介
   LINK: process.env.NEXT_PUBLIC_LINK || 'https://crossborder101.com', // 网站地址
-  KEYWORDS: process.env.NEXT_PUBLIC_KEYWORD || '品牌出海, 跨境电商, 众筹, 海外公司注册, 海外银行开户, 移民, 留学, 旅行', // 网站关键词 英文逗号隔开
+  KEYWORDS: process.env.NEXT_PUBLIC_KEYWORD || '移民,留学,旅行,签证,工签,永居,入籍,海外购房,海外创业,海外公司注册,海外银行开户,美国,加拿大,墨西哥,西班牙,葡萄牙,亚美尼亚,日本,马来西亚,泰国,印度尼西亚', // 网站关键词 英文逗号隔开
 
   // 社交链接，不需要可留空白，例如 CONTACT_WEIBO:''
   CONTACT_EMAIL: process.env.NEXT_PUBLIC_CONTACT_EMAIL || '', // 邮箱地址 例如mail@tangly1024.com
