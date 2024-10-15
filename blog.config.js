c0e3db47f9c77ce567dda82d5da306af67fe2752
zhongzhihai <zhongzhihai@hotmail.com>
2024-10-15 11:08:35 +0800

    Update blog.config.js

--- blog.config.js
+++ blog.config.js
@@ -27,7 +27,7 @@ const BLOG = {
   AUTHOR: process.env.NEXT_PUBLIC_AUTHOR || '无界101', // 您的昵称 例如 tangly1024
   BIO: process.env.NEXT_PUBLIC_BIO || '风景这边独好', // 作者简介
   LINK: process.env.NEXT_PUBLIC_LINK || 'https://borderless101.com', // 网站地址
-  KEYWORDS: process.env.NEXT_PUBLIC_KEYWORD || '移民,留学,旅行,签证,工签,永居,入籍,海外购房,海外创业,海外公司注册,海外银行开户,美国,加拿大,墨西哥,西班牙,葡萄牙,亚美尼亚,日本,马来西亚,泰国,印度尼西亚', // 网站关键词 英文逗号隔开
+  KEYWORDS: process.env.NEXT_PUBLIC_KEYWORD || '移民,留学,旅行,签证,工签,永居,入籍,海外购房,海外投资,海外创业,海外公司注册,海外银行开户,离岸账户,美股券商,海外电话卡,美国,加拿大,墨西哥,西班牙,葡萄牙,芬兰,亚美尼亚,日本,马来西亚,泰国,印度尼西亚', // 网站关键词 英文逗号隔开
 
   // 社交链接，不需要可留空白，例如 CONTACT_WEIBO:''
   CONTACT_EMAIL: process.env.NEXT_PUBLIC_CONTACT_EMAIL || '', // 邮箱地址 例如mail@tangly1024.com
